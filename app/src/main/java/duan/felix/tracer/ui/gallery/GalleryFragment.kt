f000ee4f7df2f713c8859df3637c51eeacb0b165
Felix <duanyufei@dayuwuxian.com>
2019-12-25 16:21:29 +0800

    Add Gallery & TODOs

---
+++ app/src/main/java/duan/felix/tracer/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,12 @@
+package duan.felix.tracer.ui.gallery
+
+import androidx.fragment.app.Fragment
+
+// TODO: make gridview for images
+// TODO: load images into gridview
+// TODO: make selectable
+// TODO: make drag selectable
+// TODO: implement MediaPicker
+// TODO: make loading view
+class GalleryFragment : Fragment() {
+}
