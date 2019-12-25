b7e8eb21456ebbce0a974c10fb69f7cf9a2fb320
Felix <duanyufei@dayuwuxian.com>
2019-12-25 11:21:01 +0800

    Make custom mark icons

--- app/src/main/java/duan/felix/tracer/presentation/SpotThumbnailRenderer.kt
+++ app/src/main/java/duan/felix/tracer/presentation/SpotThumbnailRenderer.kt
@@ -1,16 +1,27 @@
 package duan.felix.tracer.presentation
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.view.LayoutInflater
 import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.Marker
 import com.google.maps.android.clustering.Cluster
 import com.google.maps.android.clustering.ClusterManager
 import com.google.maps.android.clustering.view.DefaultClusterRenderer
 import com.google.maps.android.ui.IconGenerator
 import duan.felix.tracer.R
+import duan.felix.tracer.app.TracerApp
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 
 class SpotThumbnailRenderer(
@@ -23,28 +34,85 @@ class SpotThumbnailRenderer(
   private val mClusterIconGenerator = IconGenerator(context.applicationContext)
   private val mImageView: ImageView
   private val mClusterImageView: ImageView
+  private val mClusterCounter: TextView
   private val layoutInflater = LayoutInflater.from(context)
 
   init {
-    var thumbnail = layoutInflater.inflate(R.layout.thumbnail_spot, null)
+    val thumbnail = layoutInflater.inflate(R.layout.thumbnail_spot, null)
     mImageView = thumbnail.findViewById(R.id.icon)
     mIconGenerator.setContentView(thumbnail)
 
-    thumbnail = layoutInflater.inflate(R.layout.thumbnail_spot_cluster, null)
-    mClusterImageView = thumbnail.findViewById(R.id.icon)
-    mClusterIconGenerator.setContentView(thumbnail)
+    val clusterThumbnail = layoutInflater.inflate(R.layout.thumbnail_spot_cluster, null)
+    mClusterImageView = clusterThumbnail.findViewById(R.id.icon)
+    mClusterCounter = clusterThumbnail.findViewById(R.id.count)
+    mClusterIconGenerator.setContentView(clusterThumbnail)
   }
 
-  override fun onBeforeClusterItemRendered(item: SpotClusterItem?, markerOptions: MarkerOptions?) {
-    val icon = mIconGenerator.makeIcon()
-    markerOptions?.icon(BitmapDescriptorFactory.fromBitmap(icon))?.title("Icon")
+  override fun onClusterItemRendered(clusterItem: SpotClusterItem?, marker: Marker?) {
+    GlobalScope.launch {
+      Glide.with(TracerApp.appContext).asBitmap().load(clusterItem?.spot?.url)
+        .addListener(object : RequestListener<Bitmap> {
+          override fun onResourceReady(
+            resource: Bitmap?,
+            model: Any?,
+            target: Target<Bitmap>?,
+            dataSource: DataSource?,
+            isFirstResource: Boolean
+          ): Boolean {
+            GlobalScope.launch(Dispatchers.Main) {
+              mImageView.setImageBitmap(resource!!)
+              val bmp = mIconGenerator.makeIcon()
+              marker?.setIcon(BitmapDescriptorFactory.fromBitmap(bmp))
+            }
+            return true
+          }
+
+          override fun onLoadFailed(
+            e: GlideException?,
+            model: Any?,
+            target: Target<Bitmap>?,
+            isFirstResource: Boolean
+          ): Boolean {
+            TODO("not implemented")
+          }
+        }).submit(BITMAP_SIZE, BITMAP_SIZE)
+    }
   }
 
-  override fun onBeforeClusterRendered(
-    cluster: Cluster<SpotClusterItem>?,
-    markerOptions: MarkerOptions?
-  ) {
-    val icon = mClusterIconGenerator.makeIcon()
-    markerOptions?.icon(BitmapDescriptorFactory.fromBitmap(icon))?.title("Cluster")
+  override fun onClusterRendered(cluster: Cluster<SpotClusterItem>?, marker: Marker?) {
+    GlobalScope.launch {
+      Glide.with(TracerApp.appContext).asBitmap().load(cluster?.items?.first()?.spot?.url)
+        .addListener(object : RequestListener<Bitmap> {
+          override fun onResourceReady(
+            resource: Bitmap?,
+            model: Any?,
+            target: Target<Bitmap>?,
+            dataSource: DataSource?,
+            isFirstResource: Boolean
+          ): Boolean {
+            GlobalScope.launch(Dispatchers.Main) {
+              // TODO: show better text. Color, 99+, etc.
+              mClusterCounter.setText(cluster?.size.toString())
+              mClusterImageView.setImageBitmap(resource!!)
+              val bmp = mClusterIconGenerator.makeIcon()
+              marker?.setIcon(BitmapDescriptorFactory.fromBitmap(bmp))
+            }
+            return true
+          }
+
+          override fun onLoadFailed(
+            e: GlideException?,
+            model: Any?,
+            target: Target<Bitmap>?,
+            isFirstResource: Boolean
+          ): Boolean {
+            TODO("not implemented")
+          }
+        }).submit(BITMAP_SIZE, BITMAP_SIZE)
+    }
+  }
+
+  companion object {
+    const val BITMAP_SIZE = 100
   }
 }
